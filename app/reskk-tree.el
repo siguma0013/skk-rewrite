e61e08ca611b20c6365f122d3c486ae4200e1db0
siguma0013 <siguma.lutetium.999@gmail.com>
2026-01-09 04:33:42 +0900

    update:木構造リファクタ

--- app/reskk-tree.el
+++ app/reskk-tree.el
@@ -1,54 +1,55 @@
-;; reskk-convert-rule.el --- skk変換ルール -*- lexical-binding: t; -*-
+;;; reskk-tree.el --- skkかな変換木構造 -*- lexical-binding: t; -*-
 
 ;;; Code:
-
 (require 'cl-lib)
 
-;; trieノード
-;; child 子ノード
-;; value 出力文字列
-;; pending 未消費文字列
-(cl-defstruct reskk-convert-tree
-  "木構造(trie)による変換テーブル"
+;; かな変換用木構造(trie)
+(defvar reskk-tree nil)
+
+;; reskk-treeのノード定義
+;; [children] 子ノード
+;; [value]    出力文字列
+;; [pending]  未消費文字列
+(cl-defstruct reskk-tree-node
   children
   value
   pending)
 
-(defun reskk-make-trie ()
-  "木構造(trie)のノード作成関数"
-  (make-reskk-convert-tree
+;; reskk-tree-node作成関数
+(defun reskk-make-node ()
+  (make-reskk-tree-node
     :children (make-hash-table :test 'equal)
     :value nil
     :pending nil)
   )
 
+;; reskk-treeへのノード登録関数
 (defun reskk-insert-trie (trie key value pending)
-  "木構造(trie)のキー登録関数"
   (let ((node trie))
     (dolist (char (string-to-list key))
       (setq node
         (or
-          (gethash char (reskk-convert-tree-children node))
+          (gethash char (reskk-tree-node-children node))
           (puthash char
-            (reskk-make-trie)
-            (reskk-convert-tree-children node)
+            (reskk-make-node)
+            (reskk-tree-node-children node)
             )
           )
         )
       )
 
     ;; 終端登録
-    (setf (reskk-convert-tree-value node) value)
-    (setf (reskk-convert-tree-pending node) pending)
+    (setf (reskk-tree-node-value node) value)
+    (setf (reskk-tree-node-pending node) pending)
     )
   )
 
+;; 木構造の検索関数(完全一致)
 (defun reskk--find-node (root key)
-  "木構造(trie)の完全一致検索関数"
   (cl-block find-node
     (let ((node root))
       (dolist (char (string-to-list key))
-        (setq node (gethash char (reskk-convert-tree-children node)))
+        (setq node (gethash char (reskk-tree-node-children node)))
         (unless node (cl-return-from find-node nil)))
 
       (cl-return-from find-node node)
@@ -56,23 +57,24 @@
     )
   )
 
+;; 規定の木構造検索関数(完全一致)
 (defun reskk-find-node (key)
-  "規定の完全一致検索関数"
   (reskk--find-node reskk-convert-trie key))
 
+;; ノードが末端であるかの判定関数
 (defun reskk-tree-is-leaf (node)
   "ノードが末端であるかの判定関数"
-  (zerop (hash-table-count (reskk-convert-tree-children node))))
+  (zerop (hash-table-count (reskk-tree-node-children node))))
 
 (defun reskk-tree-get-value (leaf)
   "ノードから出力文字列を取得するインターフェース"
-  (reskk-convert-tree-value leaf))
+  (reskk-tree-node-value leaf))
 
 (defun reskk-tree-get-pending (leaf)
   "ノードから未消費文字列を取得するインターフェース"
-  (reskk-convert-tree-pending leaf))
+  (reskk-tree-node-pending leaf))
 
-(setq reskk-convert-trie (reskk-make-trie))
+(setq reskk-convert-trie (reskk-make-node))
 
 ;; あ行
 (reskk-insert-trie reskk-convert-trie "a" "あ" nil)
